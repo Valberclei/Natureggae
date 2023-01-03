60a6a4d5b90cb03e3165d356e23edb1e9470b490
vobh_ <valberclei.campos@outlook.com>
2023-01-03 11:05:50 -0300

    Elmn invald code 01

--- app/models.py
+++ app/models.py
@@ -11,22 +11,3 @@ class PostImage(models.Model):
     cover = models.ImageField(upload_to='images/')
     def __str__(self):
       return self.title
-
-class Candidato(models.Model):
-    SEXO_CHOICES = [
-        ("M", "Masculino"),
-        ("F", "Feminino"),
-    ]
-
-    NIVEL_CHOICES = [
-        ("S", "SENIOR"),
-        ("P", "PLENO"),
-        ("J", "JUNIOR"),
-    ]
-    nome = models.CharField(max_length=50, null=False, blank=False)
-    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, null=False, blank=False)
-    idade = models.CharField(max_length=2, null=False, blank=False)
-    nivel = models.CharField(max_length=1, choices=NIVEL_CHOICES, null=False, blank=False)
-    formacao = models.CharField(max_length=50, null=False, blank=False)
-    pix = models.CharField(max_length=50, null=True, blank=True)
-    celular = models.CharField(max_length=14, null=False, blank=False)
