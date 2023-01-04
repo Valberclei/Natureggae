519710582d509258f3734672cf55d2af64994a3b
vobh_ <valberclei.campos@outlook.com>
2023-01-04 09:21:48 -0300

    Requiremnts modify

--- natureggae/settings.py
+++ natureggae/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'natureggae.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dakota',
+        'NAME': 'zappling',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
