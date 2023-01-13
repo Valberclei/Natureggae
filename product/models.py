ff04130f057874d91a9541f20850bbca4f868576
vobh_ <valberclei.campos@outlook.com>
2023-01-13 16:46:00 -0300

    Ajuste do cart/templates/partials/cart_item.html

--- product/models.py
+++ product/models.py
@@ -7,7 +7,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+
     class Meta:
+        verbose_name_plural = 'Categorias'
         ordering = ('name',)
 
     def __str__(self):
