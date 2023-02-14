13e8161ae34a2c48e1e8a7e03d7b5ff6706d481b
vobh_ <valberclei.campos@outlook.com>
2023-02-14 17:38:51 -0300

    Concluindo o campo de pesquisa de eventos

--- evento/models.py
+++ evento/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files import File
 
 class Category(models.Model):
     name = models.CharField(max_length=25)
@@ -17,6 +20,8 @@ class Evento(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
 
     class Meta:
         ordering = ('-created_at',)
@@ -26,3 +31,27 @@ class Evento(models.Model):
 
     def get_display_price(self):
         return self.price / 100
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return self.thumbnail.url
+            else:
+                return 'https://via.placeholder.com/240x240.jpg'
+
+    def make_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
